c7d666e49a94780d62a0117b24d2f190dd0b29c9
mrvalraven <akatiago@gmail.com>
2022-01-03 10:12:10 +0000

    updated url

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,7 +7,7 @@ module.exports = {
   solidity: "0.8.0",
   networks: {
     ropsten: {
-      url: "https://eth-ropsten.alchemyapi.io/v2/hNepa2j77Jz3oGcERNSduGgNXlr2RM9C",
+      url: [process.env.HOSTING_URL],
       accounts: [process.env.DEPLOYMENT_WALLET_KEY],
     },
   },
